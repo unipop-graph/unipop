15345dbba08bf8352d7a1a770bc234033f2c633f
Ran Magen <rmagen@gmail.com>
2015-03-08 02:42:43 +0200

    update feature support

--- src/main/java/com/tinkerpop/gremlin/elastic/structure/ElasticFeatures.java
+++ src/main/java/com/tinkerpop/gremlin/elastic/structure/ElasticFeatures.java
@@ -14,6 +14,96 @@ public class ElasticFeatures implements Graph.Features {
                     public boolean supportsVariables() {
                         return false;
                     }
+
+                    @Override
+                    public boolean supportsBooleanValues() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsByteValues() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsDoubleValues() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsFloatValues() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsIntegerValues() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsLongValues() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsMapValues() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsMixedListValues() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsBooleanArrayValues() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsByteArrayValues() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsDoubleArrayValues() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsFloatArrayValues() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsIntegerArrayValues() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsStringArrayValues() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsLongArrayValues() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsSerializableValues() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsStringValues() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsUniformListValues() {
+                        return false;
+                    }
                 };
             }
 
@@ -90,6 +180,42 @@ public class ElasticFeatures implements Graph.Features {
             @Override
             public boolean supportsMetaProperties() {
                 return false;
+            }
+
+            @Override
+            public VertexPropertyFeatures properties() {
+                return new VertexPropertyFeatures() {
+                    @Override
+                    public boolean supportsUserSuppliedIds() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsNumericIds() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsStringIds() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsUuidIds() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsCustomIds() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean supportsAnyIds() {
+                        return false;
+                    }
+
+                };
             }
         };
     }
