e5f4f10eaeec1f84519be632398577fca0d3b707
eliran moyal <eliran.moyal1@gmail.com>
2015-03-11 01:40:16 +0200

    first try to fix strategy. still a bit buggy

--- src/main/java/com/tinkerpop/gremlin/elastic/process/graph/traversal/sideEffect/ElasticGraphStep.java
+++ src/main/java/com/tinkerpop/gremlin/elastic/process/graph/traversal/sideEffect/ElasticGraphStep.java
@@ -1,11 +1,17 @@
 package com.tinkerpop.gremlin.elastic.process.graph.traversal.sideEffect;
 
 import com.tinkerpop.gremlin.elastic.elasticservice.ElasticService;
+import com.tinkerpop.gremlin.elastic.structure.ElasticEdge;
 import com.tinkerpop.gremlin.elastic.structure.ElasticGraph;
+import com.tinkerpop.gremlin.elastic.structure.ElasticVertex;
 import com.tinkerpop.gremlin.process.Traversal;
+import com.tinkerpop.gremlin.process.TraverserGenerator;
 import com.tinkerpop.gremlin.process.graph.step.sideEffect.GraphStep;
 import com.tinkerpop.gremlin.process.graph.util.HasContainer;
+import com.tinkerpop.gremlin.process.util.EmptyStep;
+import com.tinkerpop.gremlin.process.util.TraversalMetrics;
 import com.tinkerpop.gremlin.structure.*;
+import org.apache.lucene.queries.TermFilter;
 import org.elasticsearch.common.geo.builders.ShapeBuilder;
 import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.common.xcontent.json.JsonXContent;
@@ -15,12 +21,20 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import java.util.*;
 
 public class ElasticGraphStep<E extends Element> extends GraphStep<E> {
-    public final List<HasContainer> hasContainers = new ArrayList<HasContainer>();
+
     ElasticService elasticService;
+    Direction direction;
+    List<Object> idsExtended;
+    BoolFilterBuilder boolFilterBuilder;
 
-
-    public ElasticGraphStep(final Traversal traversal, final ElasticGraph graph, final Class<E> returnClass, Optional<String> label, final Object... ids) {
+    public final List<HasContainer> hasContainers = new ArrayList<HasContainer>();
+    public ElasticGraphStep(Direction direction,final Traversal traversal, final ElasticGraph graph, final Class<E> returnClass, Optional<String> label, final Object... ids) {
         super(traversal, graph, returnClass, ids);
+        this.direction = direction;
+        this.idsExtended = new ArrayList<>();
+        this.boolFilterBuilder = FilterBuilders.boolFilter();
+        idsExtended.addAll(Arrays.asList(ids));
+
         if (label.isPresent()) this.setLabel(label.get());
         this.setIteratorSupplier(() -> (Iterator<E>) (Vertex.class.isAssignableFrom(this.returnClass) ? this.vertices() : this.edges()));
         this.elasticService = graph.elasticService;
@@ -34,12 +48,76 @@ public class ElasticGraphStep<E extends Element> extends GraphStep<E> {
         return elasticService.searchEdges(getFilter(), label());
     }
 
-    private FilterBuilder getFilter() {
-        Object[] ids = this.getIds();
+
+    private String label() {
+        return this.getLabel().isPresent() ? this.getLabel().get() : null;
+    }
+
+    public Iterator<E> getResults(){
+        if(this.previousStep!=null && (!(this.previousStep instanceof EmptyStep))) {
+            ElasticGraphStep previousStep = (ElasticGraphStep) this.getPreviousStep();
+            Iterator<? extends Element> iterator =  previousStep.getResults();
+            List<Object> ids = new ArrayList<Object>();
+            while(iterator.hasNext()){
+                Element next = iterator.next();
+                if(next instanceof Edge ){
+                    ElasticEdge edge = (ElasticEdge) next;
+                    ids.addAll(edge.getVertexId(direction));
+                }
+                else {
+                    ids.add(next.id());
+                }
+
+            }
+
+            addPredicateAccordingToIds(ids);
+        }
+         return this.iteratorSupplier.get();
+    }
+
+    private void addPredicateAccordingToIds(List<Object> ids){
+        if(Vertex.class.isAssignableFrom(this.returnClass)){
+            //moving from edges to vertices, we don't need direction
+            this.idsExtended.addAll(ids);
+        }
+
+        else {
+            //moving from vertices to edges we need direction
+            //this is a little patchy - could have done it with adding hasContainer for In and Out but not for BOTH (no or)
+            TermsFilterBuilder inFilter =  FilterBuilders.termsFilter(ElasticEdge.InId, ids.toArray());
+            TermsFilterBuilder outFilter =  FilterBuilders.termsFilter(ElasticEdge.OutId, ids.toArray());
+            if(direction == Direction.IN) boolFilterBuilder.must(inFilter);
+            if(direction == Direction.OUT) boolFilterBuilder.must(outFilter);
+            if(direction == Direction.BOTH) boolFilterBuilder.must(FilterBuilders.orFilter(inFilter, outFilter));
+        }
+    }
+
+
+    @Override
+    public void generateTraversers(final TraverserGenerator traverserGenerator) {
+        if (PROFILING_ENABLED) TraversalMetrics.start(this);
+        try {
+            this.start = this.getResults();
+            //this is code from supers super
+            if (this.start instanceof Iterator) {
+                this.starts.add(traverserGenerator.generateIterator((Iterator<E>) this.start, this, 1l));
+            } else {
+                this.starts.add(traverserGenerator.generate((E) this.start, this, 1l));
+            }
+
+        } catch (final Exception e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        } finally {
+            if (PROFILING_ENABLED) TraversalMetrics.stop(this);
+        }
+    }
+
+
+    private  FilterBuilder getFilter(){
+        Object[] ids = this.idsExtended.toArray();
         List<HasContainer> hasContainers = this.hasContainers;
         if (hasContainers.size() == 0 && ids.length == 0) return null;
 
-        BoolFilterBuilder boolFilterBuilder = FilterBuilders.boolFilter();
         for (HasContainer has : hasContainers) {
             if (has.predicate instanceof Compare) {
                 String predicateString = has.predicate.toString();
@@ -77,7 +155,7 @@ public class ElasticGraphStep<E extends Element> extends GraphStep<E> {
         return boolFilterBuilder;
     }
 
-    private ShapeBuilder GetShapeBuilder(Object object) {
+    private  ShapeBuilder GetShapeBuilder(Object object) {
         try {
             String geoJson = (String) object;
             XContentParser parser = JsonXContent.jsonXContent.createParser(geoJson);
@@ -89,7 +167,7 @@ public class ElasticGraphStep<E extends Element> extends GraphStep<E> {
         }
     }
 
-    private String label() {
-        return this.getLabel().isPresent() ? this.getLabel().get() : null;
-    }
+
+
+
 }
--- src/main/java/com/tinkerpop/gremlin/elastic/process/graph/traversal/strategy/ElasticGraphStepStrategy.java
+++ src/main/java/com/tinkerpop/gremlin/elastic/process/graph/traversal/strategy/ElasticGraphStepStrategy.java
@@ -2,11 +2,16 @@ package com.tinkerpop.gremlin.elastic.process.graph.traversal.strategy;
 
 import com.tinkerpop.gremlin.elastic.process.graph.traversal.sideEffect.ElasticGraphStep;
 import com.tinkerpop.gremlin.elastic.structure.ElasticGraph;
+import com.tinkerpop.gremlin.elastic.structure.ElasticVertex;
 import com.tinkerpop.gremlin.process.*;
 import com.tinkerpop.gremlin.process.graph.marker.HasContainerHolder;
+import com.tinkerpop.gremlin.process.graph.step.map.EdgeVertexStep;
+import com.tinkerpop.gremlin.process.graph.step.map.VertexStep;
 import com.tinkerpop.gremlin.process.graph.step.sideEffect.*;
 import com.tinkerpop.gremlin.process.graph.strategy.AbstractTraversalStrategy;
+import com.tinkerpop.gremlin.process.util.EmptyStep;
 import com.tinkerpop.gremlin.process.util.TraversalHelper;
+import com.tinkerpop.gremlin.structure.Direction;
 
 public class ElasticGraphStepStrategy extends AbstractTraversalStrategy {
     private static final ElasticGraphStepStrategy INSTANCE = new ElasticGraphStepStrategy();
@@ -22,26 +27,47 @@ public class ElasticGraphStepStrategy extends AbstractTraversalStrategy {
         final Step<?, ?> startStep = TraversalHelper.getStart(traversal);
         if (startStep instanceof GraphStep) {
             final GraphStep<?> originalGraphStep = (GraphStep) startStep;
-            final ElasticGraphStep<?> elasticGraphStep = new ElasticGraphStep<>(originalGraphStep.getTraversal(), originalGraphStep.getGraph(ElasticGraph.class), originalGraphStep.getReturnClass(), originalGraphStep.getLabel(), originalGraphStep.getIds());
+            ElasticGraph graph = originalGraphStep.getGraph(ElasticGraph.class);
+            final ElasticGraphStep<?> elasticGraphStep = new ElasticGraphStep<>(null,originalGraphStep.getTraversal(), graph, originalGraphStep.getReturnClass(), originalGraphStep.getLabel(), originalGraphStep.getIds());
             TraversalHelper.replaceStep(startStep, (Step) elasticGraphStep, traversal);
-
+            ElasticGraphStep lastElasticGraphStep = elasticGraphStep;
             Step<?, ?> currentStep = elasticGraphStep.getNextStep();
             while (true) {
                 if (currentStep instanceof HasContainerHolder) {
-                    elasticGraphStep.hasContainers.addAll(((HasContainerHolder) currentStep).getHasContainers());
+
+                    lastElasticGraphStep.hasContainers.addAll(((HasContainerHolder) currentStep).getHasContainers());
                     if (currentStep.getLabel().isPresent()) {
                         final IdentityStep identityStep = new IdentityStep<>(traversal);
                         identityStep.setLabel(currentStep.getLabel().get());
                         TraversalHelper.insertAfterStep(identityStep, currentStep, traversal);
                     }
                     traversal.removeStep(currentStep);
-                } else if (currentStep instanceof IdentityStep) {
-                    // do nothing
-                } else {
-                    break;
+
+                } else if (currentStep instanceof VertexStep) {
+
+                    VertexStep<?> originalVertexStep = (VertexStep) currentStep;
+                     ElasticGraphStep<?> graphStep = new ElasticGraphStep<>(originalVertexStep.getDirection(),originalVertexStep.getTraversal(), graph, originalVertexStep.getReturnClass(), originalVertexStep.getLabel(),new Object[0]);
+                    lastElasticGraphStep = graphStep;
+                    TraversalHelper.replaceStep(currentStep, (Step) graphStep, traversal);
+
                 }
+                else if (currentStep instanceof EdgeVertexStep){
+
+                    EdgeVertexStep originalEdgeStep = (EdgeVertexStep) currentStep;
+                    ElasticGraphStep<?> graphStep = new ElasticGraphStep<>(originalEdgeStep.getDirection(),originalEdgeStep.getTraversal(), graph, ElasticVertex.class, originalEdgeStep.getLabel(),new Object[0]);
+                    lastElasticGraphStep = graphStep;
+                    TraversalHelper.replaceStep(currentStep, (Step) graphStep, traversal);
+                }
+
+                else if (currentStep instanceof EmptyStep) {
+                    break;
+                } else {
+                    //do nothing
+                }
+
                 currentStep = currentStep.getNextStep();
             }
+            int i=1;
         }
     }
 }
