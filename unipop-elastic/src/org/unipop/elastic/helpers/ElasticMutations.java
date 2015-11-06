6c74199ad7deb0265ed54280aa1f98cbc164a219
Ran Magen <rmagen@gmail.com>
2015-11-06 18:18:01 +0200

    cleanup ElasticMutations

--- unipop-elastic/src/org/unipop/elastic/helpers/ElasticMutations.java
+++ unipop-elastic/src/org/unipop/elastic/helpers/ElasticMutations.java
@@ -1,6 +1,5 @@
 package org.unipop.elastic.helpers;
 
-import org.apache.tinkerpop.gremlin.structure.Element;
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
 import org.elasticsearch.action.delete.DeleteRequestBuilder;
 import org.elasticsearch.action.index.IndexRequestBuilder;
@@ -8,8 +7,6 @@ import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.Client;
 import org.unipop.structure.BaseElement;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 public class ElasticMutations {
@@ -25,28 +22,18 @@ public class ElasticMutations {
         this.client = client;
     }
 
-    public void addElement(Element element, String index, String routing,  boolean create) {
+    public void addElement(BaseElement element, String index, String routing,  boolean create) {
         IndexRequestBuilder indexRequest = client.prepareIndex(index, element.label(), element.id().toString())
-                .setSource(propertiesMap(element)).setRouting(routing).setCreate(create);
+                .setSource(element.allFields()).setRouting(routing).setCreate(create);
         if(bulkRequest != null) bulkRequest.add(indexRequest);
         else indexRequest.execute().actionGet();
         revision++;
     }
 
-    private Map propertiesMap(Element element) {
-        if(element instanceof BaseElement)
-            return ((BaseElement)element).allFields();
 
-        Map<String, Object> map = new HashMap<>();
-        element.properties().forEachRemaining(property -> {
-            map.put(property.key(), property.value());
-        });
-        return map;
-    }
-
-    public void updateElement(Element element, String index, String routing, boolean upsert) throws ExecutionException, InterruptedException {
+    public void updateElement(BaseElement element, String index, String routing, boolean upsert) throws ExecutionException, InterruptedException {
         UpdateRequest updateRequest = new UpdateRequest(index, element.label(), element.id().toString())
-                .doc(propertiesMap(element)).routing(routing);
+                .doc(element.allFields()).routing(routing);
         if(upsert)
             updateRequest.detectNoop(true).docAsUpsert(true);
         if(bulkRequest != null) bulkRequest.add(updateRequest);
@@ -55,7 +42,7 @@ public class ElasticMutations {
     }
 
 
-    public void deleteElement(Element element, String index, String routing) {
+    public void deleteElement(BaseElement element, String index, String routing) {
         DeleteRequestBuilder deleteRequestBuilder = client.prepareDelete(index, element.label(), element.id().toString()).setRouting(routing);
         if(bulkRequest != null) bulkRequest.add(deleteRequestBuilder);
         else deleteRequestBuilder.execute().actionGet();
