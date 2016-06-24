54a7513452fef0558565bc733bcc75e00afe5d05
sbarzilay <seanbarzilay@github.com>
2016-06-24 17:33:51 +0300

    converted UniGraphCoalesceStep, UniGraphUnionStep to implement UniBulkStep

--- unipop-core/src/org/unipop/process/union/UniGraphUnionStepStrategy.java
+++ unipop-core/src/org/unipop/process/union/UniGraphUnionStepStrategy.java
@@ -10,6 +10,8 @@ import org.apache.tinkerpop.gremlin.process.traversal.step.map.SelectStep;
 import org.apache.tinkerpop.gremlin.process.traversal.step.util.ReducingBarrierStep;
 import org.apache.tinkerpop.gremlin.process.traversal.strategy.AbstractTraversalStrategy;
 import org.apache.tinkerpop.gremlin.process.traversal.util.TraversalHelper;
+import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.unipop.structure.UniGraph;
 
 /**
  * Created by sbarzilay on 3/23/16.
@@ -17,6 +19,13 @@ import org.apache.tinkerpop.gremlin.process.traversal.util.TraversalHelper;
 public class UniGraphUnionStepStrategy extends AbstractTraversalStrategy<TraversalStrategy.ProviderOptimizationStrategy> implements TraversalStrategy.ProviderOptimizationStrategy {
     @Override
     public void apply(Traversal.Admin<?, ?> traversal) {
+        Graph graph = traversal.getGraph().get();
+        if (!(graph instanceof UniGraph)) {
+            return;
+        }
+
+        UniGraph uniGraph = (UniGraph) graph;
+
         TraversalHelper.getStepsOfClass(UnionStep.class, traversal).forEach(unionStep -> {
             Traversal.Admin[] traversals = (Traversal.Admin[]) unionStep.getGlobalChildren().toArray(new Traversal.Admin[0]);
             for (Traversal.Admin admin : traversals) {
@@ -26,7 +35,7 @@ public class UniGraphUnionStepStrategy extends AbstractTraversalStrategy<Travers
                         traversal.getParent() instanceof RepeatStep)
                     return;
             }
-            UniGraphUnionStep uniGraphUnionStep = new UniGraphUnionStep(traversal, traversals);
+            UniGraphUnionStep uniGraphUnionStep = new UniGraphUnionStep(traversal, uniGraph, traversals);
             TraversalHelper.replaceStep(unionStep, uniGraphUnionStep, traversal);
         });
     }
