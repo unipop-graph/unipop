fb241db009d304a0f39893c05748679cda040f83
sbarzilay <sesnbarzilay@gmail.com>
2016-07-12 02:27:45 -0500

    fixed problems in MultiFieldPropertySchema and added tests

--- unipop-core/src/org/unipop/schema/property/MultiFieldPropertySchema.java
+++ unipop-core/src/org/unipop/schema/property/MultiFieldPropertySchema.java
@@ -1,8 +1,13 @@
 package org.unipop.schema.property;
 
+import org.apache.tinkerpop.gremlin.process.traversal.P;
+import org.apache.tinkerpop.gremlin.process.traversal.step.util.HasContainer;
 import org.unipop.query.predicates.PredicatesHolder;
+import org.unipop.query.predicates.PredicatesHolderFactory;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class MultiFieldPropertySchema implements PropertySchema {
@@ -19,9 +24,9 @@ public class MultiFieldPropertySchema implements PropertySchema {
     @Override
     public Map<String, Object> toProperties(Map<String, Object> source) {
         String finalValue = null;
-        for(String field : fields){
+        for (String field : fields) {
             Object value = source.get(field);
-            if(value == null) return null;
+            if (value == null) return Collections.emptyMap();
             finalValue = finalValue == null ? value.toString() : finalValue + delimiter + value.toString();
         }
         return Collections.singletonMap(key, finalValue);
@@ -39,25 +44,61 @@ public class MultiFieldPropertySchema implements PropertySchema {
 
     @Override
     public Map<String, Object> toFields(Map<String, Object> properties) {
-        return Collections.emptyMap();
 //        Object value = properties.get(this.key);
-//        if(value == null) return null;
+//        if (value == null) return Collections.emptyMap();
 //        Map<String, Object> result = new HashMap<>(fields.size());
 //        String[] values = value.toString().split(delimiter);
-//        //TODO: what if values.length != fields.length ??? o_O
-//        for(int i = 0; i < fields.size(); i++) {
-//            result.put(fields.get(i), values[i]);
-//        }
+//            //TODO: what if values.length != fields.length ??? o_O
+//            for (int i = 0; i < fields.size(); i++) {
+//                result.put(fields.get(i), values[i]);
+//            }
 //        return result;
+        return Collections.emptyMap();
     }
 
     @Override
     public Set<String> toFields(Set<String> propertyKeys) {
-        return Collections.emptySet();
+        return propertyKeys.contains(key) ? new HashSet<>(fields) : Collections.emptySet();
     }
 
     @Override
     public PredicatesHolder toPredicates(PredicatesHolder predicatesHolder) {
-        return null;
+        Stream<HasContainer> hasContainers = predicatesHolder.findKey(this.key);
+
+        Set<PredicatesHolder> predicateHolders = hasContainers.map(this::toPredicate).collect(Collectors.toSet());
+        return PredicatesHolderFactory.create(predicatesHolder.getClause(), predicateHolders);
+    }
+
+    private void addToList(Map<String, List> map, String key, Object value) {
+        if (!map.containsKey(key))
+            map.put(key, new ArrayList());
+        map.get(key).add(value);
+    }
+
+    private PredicatesHolder toPredicate(HasContainer has) {
+        String[] keys = fields.toArray(new String[fields.size()]);
+        Object value = has.getValue();
+        Set<HasContainer> predicates = new HashSet<>();
+        if (value instanceof String) {
+            String valueString = value.toString();
+            String[] values = valueString.split(delimiter);
+            for (int i = 0; i < keys.length; i++) {
+                P predicate = has.getPredicate().clone();
+                final Object currentValue = values[i];
+                P p = new P(predicate.getBiPredicate(), currentValue);
+                predicates.add(new HasContainer(keys[i], p));
+            }
+        } else if (value instanceof Collection) {
+            Collection values = (Collection) value;
+            Map<String, List> predicatesValues = new HashMap<>();
+            values.forEach(v -> {
+                String[] split = v.toString().split(delimiter);
+                for (int i = 0; i < keys.length; i++) {
+                    addToList(predicatesValues, keys[i], split[i]);
+                }
+            });
+            predicatesValues.entrySet().forEach(kv -> predicates.add(new HasContainer(kv.getKey(), new P(has.getBiPredicate(), kv.getValue()))));
+        }
+        return PredicatesHolderFactory.and(predicates.toArray(new HasContainer[predicates.size()]));
     }
 }
--- unipop-test/test/org/unipop/MultiFieldTests.java
+++ unipop-test/test/org/unipop/MultiFieldTests.java
@@ -3,21 +3,115 @@ package org.unipop;
 import org.apache.tinkerpop.gremlin.AbstractGremlinTest;
 import org.apache.tinkerpop.gremlin.GraphManager;
 import org.apache.tinkerpop.gremlin.LoadGraphWith;
-import org.junit.Before;
+import org.apache.tinkerpop.gremlin.process.traversal.P;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.structure.Edge;
+import org.apache.tinkerpop.gremlin.structure.T;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.apache.tinkerpop.gremlin.structure.VertexProperty;
+import org.apache.tinkerpop.gremlin.util.iterator.IteratorUtils;
 import org.junit.Test;
 import org.unipop.elastic.test.ElasticGraphProvider;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by sbarzilay on 7/7/16.
  */
-public class MultiFieldTests extends AbstractGremlinTest{
+public class MultiFieldTests extends AbstractGremlinTest {
     public MultiFieldTests() throws Exception {
         GraphManager.setGraphProvider(new ElasticGraphProvider());
     }
 
-    @LoadGraphWith
+    @LoadGraphWith(LoadGraphWith.GraphData.MODERN)
     @Test
-    public void name() throws Exception {
+    public void g_V_hasXname_markoX_out() throws Exception {
+        GraphTraversal<Vertex, Vertex> t = g.V().has("name", "marko").out();
 
+        List<Vertex> vertices = t.toList();
+
+        assertEquals(3, vertices.size());
+
+        Map<String, Vertex> idToVertex = new HashMap<>();
+
+        vertices.forEach(vertex -> idToVertex.put(vertex.id().toString(), vertex));
+
+        assertTrue(idToVertex.containsKey("4"));
+        assertEquals("josh_32", idToVertex.get("4").property("name_age").value().toString());
+        assertTrue(idToVertex.containsKey("2"));
+        assertEquals("vadas_27", idToVertex.get("2").property("name_age").value().toString());
+        assertTrue(idToVertex.containsKey("3"));
+        assertFalse(idToVertex.get("3").property("name_age").isPresent());
+    }
+
+    @LoadGraphWith(LoadGraphWith.GraphData.MODERN)
+    @Test
+    public void g_V_out() throws Exception {
+        GraphTraversal<Vertex, Vertex> t = g.V().out();
+
+        List<Vertex> vertices = t.toList();
+
+        assertEquals(6, vertices.size());
+
+        Map<String, List<Vertex>> idToVertices = vertices.stream()
+                .collect(Collectors.groupingBy(vertex -> vertex.id().toString()));
+
+        assertTrue(idToVertices.containsKey("2"));
+        assertEquals(1, idToVertices.get("2").size());
+        assertTrue(idToVertices.containsKey("3"));
+        assertEquals(3, idToVertices.get("3").size());
+        assertTrue(idToVertices.containsKey("4"));
+        assertEquals(1, idToVertices.get("4").size());
+        assertTrue(idToVertices.containsKey("5"));
+        assertEquals(1, idToVertices.get("5").size());
+
+        idToVertices.get("5").forEach(vertex -> assertFalse(vertex.property("name_age").isPresent()));
+        idToVertices.get("3").forEach(vertex -> assertFalse(vertex.property("name_age").isPresent()));
+        idToVertices.get("4").forEach(vertex -> assertEquals("josh_32", vertex.property("name_age").value().toString()));
+        idToVertices.get("2").forEach(vertex -> assertEquals("vadas_27", vertex.property("name_age").value().toString()));
+    }
+
+    @LoadGraphWith(LoadGraphWith.GraphData.MODERN)
+    @Test
+    public void g_V_hasXname_age_marko_29X() throws Exception {
+        GraphTraversal<Vertex, Vertex> t = g.V().has("name_age", "marko_29");
+        List<Vertex> vertices = t.toList();
+        assertEquals(1, vertices.size());
+        assertTrue(vertices.get(0).id().equals("1"));
+        List<VertexProperty<Object>> properties = IteratorUtils.<VertexProperty<Object>>asList(vertices.get(0).properties());
+        assertEquals(3, properties.size());
+    }
+
+    @LoadGraphWith(LoadGraphWith.GraphData.MODERN)
+    @Test
+    public void g_V_hasXname_age_within_marko_29_vadas_27X_order_byXidX() throws Exception {
+        GraphTraversal<Vertex, Vertex> t = g.V().has("name_age", P.within("marko_29", "vadas_27")).order().by(T.id);
+        List<Vertex> vertices = t.toList();
+        assertEquals(2, vertices.size());
+        assertTrue(vertices.get(0).id().equals("1"));
+        assertTrue(vertices.get(1).id().equals("2"));
+        vertices.forEach(vertex -> {
+            List<VertexProperty<Object>> properties = IteratorUtils.<VertexProperty<Object>>asList(vertex.properties());
+            assertEquals(3, properties.size());
+        });
+    }
+
+    @LoadGraphWith(LoadGraphWith.GraphData.MODERN)
+    @Test
+    public void g_EX7_05X() throws Exception {
+        GraphTraversal<Edge, Edge> e = g.E("7_0.5");
+        List<Edge> edges = e.toList();
+        assertEquals(1, edges.size());
+        Edge edge = edges.get(0);
+        assertEquals("7_0.5", edge.id().toString());
+        assertEquals("0.5", edge.property("weight").value().toString());
     }
 }
