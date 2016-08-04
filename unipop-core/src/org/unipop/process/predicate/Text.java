e9d2f81375639b4e0d1e085f080580a72d7e0dc5
sbarzilay <seanbarzilay@github.com>
2016-08-04 20:49:00 +0300

    started adding back the TextPredicate

--- unipop-core/src/org/unipop/process/predicate/Text.java
+++ unipop-core/src/org/unipop/process/predicate/Text.java
@@ -8,126 +8,128 @@ import java.util.function.BiPredicate;
 /**
  * Created by sbarzilay on 12/15/15.
  */
-public enum Text implements BiPredicate<Object, Object> {
-    PREFIX{
-        @Override
-        public boolean test(final Object first, final Object second) {
-            return second.toString().matches(first.toString().replace("?", ".?").replace("*", ".*?"));
-        }
+public class Text {
+    public static <V> P<V> like(final V value) { return new P(TextPredicate.LIKE, value); }
+    public static <V> P<V> unlike(final V value) { return new P(TextPredicate.UNLIKE, value); }
+    public static <V> P<V> regexp(final V value) { return new P(TextPredicate.REGEXP, value); }
+    public static <V> P<V> unregexp(final V value) { return new P(TextPredicate.UNREGEXP, value); }
+    public static <V> P<V> fuzzy(final V value) { return new P(TextPredicate.FUZZY, value); }
+    public static <V> P<V> unfuzzy(final V value) { return new P(TextPredicate.UNFUZZY, value); }
+    public static <V> P<V> prefix(final V value) { return new P(TextPredicate.PREFIX, value); }
+    public static <V> P<V> unprefix(final V value) { return new P(TextPredicate.UNPREFIX, value); }
 
-        /**
-         * The negative of {@code LIKE} is {@link #UNLIKE}.
-         */
-        @Override
-        public Text negate() {
-            return UNREGEXP;
-        }
-    },
-    UNPREFIX{
-        @Override
-        public boolean test(final Object first, final Object second) {
-            return !second.toString().matches(first.toString().replace("?", ".?").replace("*", ".*?"));
-        }
+    public enum TextPredicate implements BiPredicate<Object, Object> {
+        PREFIX {
+            @Override
+            public boolean test(final Object first, final Object second) {
+                return first.toString().startsWith(second.toString());
+            }
 
-        /**
-         * The negative of {@code LIKE} is {@link #UNLIKE}.
-         */
-        @Override
-        public Text negate() {
-            return PREFIX;
-        }
-    },
-    LIKE {
-        @Override
-        public boolean test(final Object first, final Object second) {
-            return second.toString().matches(first.toString().replace("?", ".?").replace("*", ".*?"));
-        }
+            /**
+             * The negative of {@code LIKE} is {@link #UNLIKE}.
+             */
+            @Override
+            public TextPredicate negate() {
+                return UNREGEXP;
+            }
+        },
+        UNPREFIX {
+            @Override
+            public boolean test(final Object first, final Object second) {
+                return !negate().test(first, second);
+            }
 
-        /**
-         * The negative of {@code LIKE} is {@link #UNLIKE}.
-         */
-        @Override
-        public Text negate() {
-            return UNLIKE;
-        }
-    },
-    UNLIKE {
-        @Override
-        public boolean test(final Object first, final Object second) {
-            return !second.toString().matches(first.toString().replace("?", ".?").replace("*", ".*?"));
-        }
+            /**
+             * The negative of {@code LIKE} is {@link #UNLIKE}.
+             */
+            @Override
+            public TextPredicate negate() {
+                return PREFIX;
+            }
+        },
+        LIKE {
+            @Override
+            public boolean test(final Object first, final Object second) {
+                return first.toString().matches(second.toString().replace("?", ".?").replace("*", ".*?"));
+            }
 
-        /**
-         * The negative of {@code UNLIKE} is {@link #LIKE}.
-         */
-        @Override
-        public Text negate() {
-            return LIKE;
-        }
-    },
-    REGEXP {
-        @Override
-        public boolean test(final Object first, final Object second) {
-            return second.toString().matches(first.toString());
-        }
+            /**
+             * The negative of {@code LIKE} is {@link #UNLIKE}.
+             */
+            @Override
+            public TextPredicate negate() {
+                return UNLIKE;
+            }
+        },
+        UNLIKE {
+            @Override
+            public boolean test(final Object first, final Object second) {
+                return !negate().test(first, second);
+            }
 
-        /**
-         * The negative of {@code REGEXP} is {@link #UNREGEXP}.
-         */
-        @Override
-        public Text negate() {
-            return UNREGEXP;
-        }
-    },
-    UNREGEXP {
-        @Override
-        public boolean test(final Object first, final Object second) {
-            return !second.toString().matches(first.toString());
-        }
+            /**
+             * The negative of {@code UNLIKE} is {@link #LIKE}.
+             */
+            @Override
+            public TextPredicate negate() {
+                return LIKE;
+            }
+        },
+        REGEXP {
+            @Override
+            public boolean test(final Object first, final Object second) {
+                return first.toString().matches(second.toString());
+            }
 
-        /**
-         * The negative of {@code UNRGEXP} is {@link #REGEXP}.
-         */
-        @Override
-        public Text negate() {
-            return REGEXP;
-        }
-    },
-    FUZZY {
-        @Override
-        public boolean test(final Object first, final Object second) {
-            throw new NotImplementedException("Fuzzy search only implemented in elastic controllers");
-        }
+            /**
+             * The negative of {@code REGEXP} is {@link #UNREGEXP}.
+             */
+            @Override
+            public TextPredicate negate() {
+                return UNREGEXP;
+            }
+        },
+        UNREGEXP {
+            @Override
+            public boolean test(final Object first, final Object second) {
+                return !negate().test(first, second);
+            }
 
-        /**
-         * The negative of {@code FUZZY} is {@link #UNFUZZY}.
-         */
-        @Override
-        public Text negate() {
-            return UNFUZZY;
-        }
-    },
-    UNFUZZY {
-        @Override
-        public boolean test(final Object first, final Object second) {
-            throw new NotImplementedException("Fuzzy search only implemented in elastic controllers");
-        }
+            /**
+             * The negative of {@code UNRGEXP} is {@link #REGEXP}.
+             */
+            @Override
+            public TextPredicate negate() {
+                return REGEXP;
+            }
+        },
+        FUZZY {
+            @Override
+            public boolean test(final Object first, final Object second) {
+                throw new NotImplementedException("Fuzzy search only implemented in elastic controllers");
+            }
 
-        /**
-         * The negative of {@code UNFUZZY} is {@link #FUZZY}.
-         */
-        @Override
-        public Text negate() {
-            return FUZZY;
-        }
-    };
+            /**
+             * The negative of {@code FUZZY} is {@link #UNFUZZY}.
+             */
+            @Override
+            public TextPredicate negate() {
+                return UNFUZZY;
+            }
+        },
+        UNFUZZY {
+            @Override
+            public boolean test(final Object first, final Object second) {
+                throw new NotImplementedException("Fuzzy search only implemented in elastic controllers");
+            }
 
-    public static <V> P<V> like(final V value) { return new P(Text.LIKE, value); }
-    public static <V> P<V> unlike(final V value) { return new P(Text.UNLIKE, value); }
-    public static <V> P<V> regexp(final V value) { return new P(Text.REGEXP, value); }
-    public static <V> P<V> unregexp(final V value) { return new P(Text.UNREGEXP, value); }
-    public static <V> P<V> fuzzy(final V value) { return new P(Text.FUZZY, value); }
-    public static <V> P<V> unfuzzy(final V value) { return new P(Text.UNFUZZY, value); }
-    public static <V> P<V> prefix(final V value) { return new P(Text.PREFIX, value); }
-    public static <V> P<V> unprefix(final V value) { return new P(Text.UNPREFIX, value); }
+            /**
+             * The negative of {@code UNFUZZY} is {@link #FUZZY}.
+             */
+            @Override
+            public TextPredicate negate() {
+                return FUZZY;
+            }
+        };
+    }
 }
