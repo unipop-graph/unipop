ec414b3f5e7b4b9612f14a10cbcfb08942ca8a19
sbarzilay <sesnbarzilay@gmail.com>
2016-10-05 06:16:06 -0500

    reverted back to JdbcGraphProvider

--- unipop-jdbc/test/org/unipop/jdbc/suite/JdbcProcessSuite.java
+++ unipop-jdbc/test/org/unipop/jdbc/suite/JdbcProcessSuite.java
@@ -2,6 +2,7 @@ package org.unipop.jdbc.suite;
 
 import org.apache.tinkerpop.gremlin.GraphProviderClass;
 import org.junit.runner.RunWith;
+import org.unipop.jdbc.JdbcSourceProvider;
 import test.JdbcGraphProvider;
 import org.unipop.structure.UniGraph;
 import org.unipop.test.UnipopProcessSuite;
@@ -12,6 +13,6 @@ import test.JdbcOptimizedGraphProvider;
  * @since 6/20/2016
  */
 @RunWith(UnipopProcessSuite.class)
-@GraphProviderClass(provider = JdbcOptimizedGraphProvider.class, graph = UniGraph.class)
+@GraphProviderClass(provider = JdbcGraphProvider.class, graph = UniGraph.class)
 public class JdbcProcessSuite {
 }
