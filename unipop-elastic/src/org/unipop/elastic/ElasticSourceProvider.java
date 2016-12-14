3e244c7e7c9d92d6ecbe9e50d5d5110e91fe5929
sbarzilay <seanbarzilay@github.com>
2016-12-14 17:53:35 +0200

    added InnerPropertySchema in unipop-elastic

--- unipop-elastic/src/org/unipop/elastic/ElasticSourceProvider.java
+++ unipop-elastic/src/org/unipop/elastic/ElasticSourceProvider.java
@@ -3,20 +3,20 @@ package org.unipop.elastic;
 import com.google.common.collect.Sets;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.unipop.elastic.common.ElasticClient;
+import org.unipop.elastic.document.DocumentController;
 import org.unipop.elastic.document.DocumentSchema;
 import org.unipop.elastic.document.schema.DocEdgeSchema;
 import org.unipop.elastic.document.schema.DocVertexSchema;
 import org.unipop.elastic.document.schema.property.IndexPropertySchema;
-import org.unipop.schema.property.PropertySchema;
-import org.unipop.util.ConversionUtils;
-import org.unipop.elastic.common.ElasticClient;
-import org.unipop.elastic.document.DocumentController;
+import org.unipop.elastic.document.schema.property.InnerPropertySchema;
 import org.unipop.query.controller.SourceProvider;
 import org.unipop.query.controller.UniQueryController;
+import org.unipop.schema.property.PropertySchema;
 import org.unipop.structure.UniGraph;
-import org.unipop.util.PropertySchemaFactory;
+import org.unipop.util.ConversionUtils;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -49,7 +49,7 @@ public class ElasticSourceProvider implements SourceProvider {
 
     @Override
     public List<PropertySchema.PropertySchemaBuilder> providerBuilders() {
-        return Collections.singletonList(new IndexPropertySchema.Builder());
+        return Arrays.asList(new IndexPropertySchema.Builder(), new InnerPropertySchema.Builder());
     }
 
     protected DocVertexSchema createVertexSchema(JSONObject vertexJson) throws JSONException {
