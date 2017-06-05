58262ef02cce975c66f2a19abc514dee93502214
sbarzilay <seanbarzilay@github.com>
2017-06-05 13:09:41 +0300

    update DeferredVertexQuery

--- unipop-core/src/org/unipop/query/search/DeferredVertexQuery.java
+++ unipop-core/src/org/unipop/query/search/DeferredVertexQuery.java
@@ -1,7 +1,12 @@
 package org.unipop.query.search;
 
+import com.google.common.collect.Sets;
 import org.apache.tinkerpop.gremlin.process.traversal.Order;
+import org.apache.tinkerpop.gremlin.process.traversal.P;
 import org.apache.tinkerpop.gremlin.process.traversal.Traversal;
+import org.apache.tinkerpop.gremlin.process.traversal.step.util.HasContainer;
+import org.apache.tinkerpop.gremlin.structure.Element;
+import org.apache.tinkerpop.gremlin.structure.T;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.javatuples.Pair;
 import org.unipop.query.predicates.PredicatesHolder;
@@ -13,6 +18,7 @@ import org.unipop.query.controller.UniQueryController;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DeferredVertexQuery extends SearchQuery<Vertex> {
     private List<DeferredVertex> vertices;
@@ -24,6 +30,16 @@ public class DeferredVertexQuery extends SearchQuery<Vertex> {
 
     public List<DeferredVertex> getVertices() {
         return this.vertices;
+    }
+
+    @Override
+    public PredicatesHolder getPredicates() {
+        Set<Object> ids = vertices.stream().map(Element::id).collect(Collectors.toSet());
+        Set<Object> labels = vertices.stream().map(Element::label).collect(Collectors.toSet());
+        HasContainer id = new HasContainer(T.id.getAccessor(), P.within(ids));
+        HasContainer label = new HasContainer(T.label.getAccessor(), P.within(labels));
+        return PredicatesHolderFactory
+                .createFromPredicates(PredicatesHolder.Clause.And, Sets.newHashSet(id, label));
     }
 
     public interface DeferredVertexController extends UniQueryController {
