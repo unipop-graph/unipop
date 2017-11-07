c50bb1d0d69ff0f51d9e11727e9b91cc614caef9
pavelbabiy <babiy8@gmail.com>
2017-11-07 14:51:29 +0200

    add embedded elasticsearch and is now working with es 5.3.1

--- unipop-elastic/src/test/LocalNode.java
+++ unipop-elastic/src/test/LocalNode.java
@@ -6,50 +6,49 @@ import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.common.unit.TimeValue;
-import org.elasticsearch.node.InternalSettingsPreparer;
-import org.elasticsearch.node.Node;
-//import org.elasticsearch.node.NodeBuilder;
 import org.elasticsearch.node.NodeValidationException;
-import org.elasticsearch.plugins.Plugin;
-import org.elasticsearch.transport.Netty4Plugin;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import pl.allegro.tech.embeddedelasticsearch.EmbeddedElastic;
+import pl.allegro.tech.embeddedelasticsearch.PopularProperties;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Collection;
-
-import static org.apache.tinkerpop.gremlin.util.iterator.IteratorUtils.asList;
+import java.net.InetAddress;
 
 public class LocalNode {
-    private final Node node;
     private final Client client;
+    private final EmbeddedElastic elastic;
 
-    public LocalNode(File dataPath) throws NodeValidationException {
+    public LocalNode(File dataPath) throws NodeValidationException, IOException, InterruptedException {
         try {
             FileUtils.deleteDirectory(dataPath);
         } catch (IOException e) {
             e.printStackTrace();
         }
-//
-//        Settings.Builder elasticsearchSettings = Settings.settingsBuilder()
-//                .put("path.data", dataPath)
-//                .put("path.home", "./data/")
-//                .put("index.number_of_shards", "1")
-//                .put("index.number_of_replicas", "0")
-//                .put("discovery.zen.ping.multicast.enabled", "false")
-//                .put("script.inline", "true")
-//                .put("script.indexed", "true")
-//                .put("script.update", "true")
-//                .put("script.groovy.sandbox.enabled", "true");
-//
-//        this.node = NodeBuilder.nodeBuilder()
-//                .local(true)
-//                .settings(elasticsearchSettings.build())
-//                .node();
 
-        this.node = elasticSearchTestNode(dataPath);
+        this.elastic = EmbeddedElastic.builder()
+                .withElasticVersion("5.3.1")
+                .withSetting("path.data", dataPath)
+                .withSetting("path.home", "./data/")
+//                .withSetting("index.number_of_shards", "1")
+//                .withSetting("index.number_of_replicas", "0")
+//                .withSetting("discovery.zen.ping.multicast.enabled", "false")
+//                .withSetting("script.inline", "true")
+//                .withSetting("script.indexed", "true")
+//                .withSetting("script.update", "true")
+//                .withSetting("script.groovy.sandbox.enabled", "true")
+                .withSetting(PopularProperties.TRANSPORT_TCP_PORT, 9350)
+                .withSetting(PopularProperties.CLUSTER_NAME, "unipop")
+                .build();
 
-        this.client = node.client();
+        elastic.start();
+
+
+        this.client = new PreBuiltTransportClient(Settings.builder().put("cluster.name", "unipop").build())
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9350));
+
         checkHealth();
     }
 
@@ -69,37 +68,5 @@ public class LocalNode {
 
     public Client getClient() {
         return client;
-    }
-
-    public Node getNode() {
-        return node;
-    }
-    public Node elasticSearchTestNode(File dataPath) throws NodeValidationException {
-        Node node = new MyNode(
-                Settings.builder()
-                        .put("transport.type", "netty4")
-                        .put("http.type", "netty4")
-                        .put("http.enabled", "true")
-                        //  .put("path.home", "elasticsearch-data")
-                        .put("path.data", dataPath)
-                        .put("path.home", "./data/")
-//                        .put("index.number_of_shards", "1")
-//                        .put("index.number_of_replicas", "0")
-//                        .put("discovery.zen.ping.multicast.enabled", "false")
-//                        .put("script.inline", "true")
-//                        .put("script.indexed", "true")
-//                        .put("script.update", "true")
-//                        .put("script.groovy.sandbox.enabled", "true")
-                        .build(),
-                asList(Netty4Plugin.class));
-        node.start();
-        return node;
-    }
-
-
-    private static class MyNode extends Node {
-        public MyNode(Settings preparedSettings, Collection<Class<? extends Plugin>> classpathPlugins) {
-            super(InternalSettingsPreparer.prepareEnvironment(preparedSettings, null), classpathPlugins);
-        }
     }
 }
